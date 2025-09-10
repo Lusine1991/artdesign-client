77bb69991b71cdc26b0f78e6d26a8380b0222817
Lusine1991 <M.L.R.777@yandex.ru>
2025-09-10 10:40:35 +0300

    upd

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://ArtDesignGevorgyans.mooo.com",
+        destination: "https://ArtDesignGevorgyans.mooo.com/api/:path*",
       },
     ];
   },
