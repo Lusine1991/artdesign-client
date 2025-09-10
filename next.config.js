8f85bde01f51dc52c808347e7ab6ef49bed23a4d
Lusine1991 <M.L.R.777@yandex.ru>
2025-09-10 10:17:06 +0300

    upd git commit -m upd

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "/",
+        destination: "https://ArtDesignGevorgyans.mooo.com",
       },
     ];
   },
